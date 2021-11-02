cfddb20ce7bafe91d77b07b5d23546ec79206014
selim.tekinarslan <selim.tekinarslan@hepsiburada.com>
2021-11-02 20:20:36 +0300

    readme added.

---
+++ README.md
@@ -0,0 +1,17 @@
+* Bottom nav da user ın ana kategorileri mevcut.
+* Her bottom nav item ı kendi içinde bir graph a sahip bizim UX de.
+* Burada amacımız, kullanıcı aşağıdaki path i izlediğinde cihazın back tuşu ile stack de doğru şekilde geri dönmesi.
+
+-flow 
+
+1- A bottom tab ile başlar
+2- A1 ekranına gider
+3- A2 ekranına gider
+4- A3 ekranına gider
+
+5- B bottom tabına geçer.
+6- B1  ekranına gider
+7- B2  ekranına gider
+8- B3  ekranına gider
+
+9- cihazın back tuşu ile geri gelmeye çalıştıgında B-stack i bitince A kısmında A1 den başlıyor sonra A3 e geçiyor sistem.
--- app/src/main/java/com/example/multiplenavgraphwithbottomnav/testui/TestFragment.kt
+++ app/src/main/java/com/example/multiplenavgraphwithbottomnav/testui/TestFragment.kt
@@ -1,51 +1,19 @@
 package com.example.multiplenavgraphwithbottomnav.testui
 
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavDestination
-import androidx.navigation.navOptions
-import com.example.multiplenavgraphwithbottomnav.R
+class A1Fragment : TestBaseFragment("A1") {
 
-
-object Const {
-    var isCheck = false
-}
-
-
-class A1Fragment : TestBaseFragment("A1", true) {
-    override fun onBackPressed() {
-        navController.navigate(R.id.a4_fragment,null, navOptions {
-            popUpTo(R.id.a2_fragment){
-                inclusive = false
-            }
-        })
-    }
 }
 
 class A2Fragment : TestBaseFragment("A2") {
 
 }
+
 class A3Fragment : TestBaseFragment("A3") {
-    override fun onBackPressed() {
-        if (!Const.isCheck){
-            Const.isCheck = true
 
-           val nd = NavDestination("com.example.multiplenavgraphwithbottomnav.testui.A4Fragment").apply {
-                label = "sefa"
-                id = 1234
-            }
-
-            val bsa = NavBackStackEntry.create(requireContext(), nd)
-            navController.backQueue.add(navController.backQueue.size-1, bsa)
-        }else{
-            navController.popBackStack()
-        }
-    }
 }
 
 class A4Fragment : TestBaseFragment("A4") {
-    override fun onBackPressed() {
-        navController.popBackStack(R.id.a2_fragment, false)
-    }
+
 }
 
 class B1Fragment : TestBaseFragment("B1") {
@@ -57,12 +25,7 @@ class B2Fragment : TestBaseFragment("B2") {
 }
 
 class B3Fragment : TestBaseFragment("B3") {
-    override fun onBackPressed() {
 
-        navController.popBackStack(R.id.a2_fragment, false)
-
-
-    }
 }
 
 class B4Fragment : TestBaseFragment("B4") {
